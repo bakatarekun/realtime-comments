b4cc48897d648056392b271604bae00cd04c6634
bakatarekun <ckyamanaka@gmail.com>
2019-09-14 12:42:57 -0400

    add comments 777

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-// Test comments 123
+// Test comments 777
 
 serviceWorker.unregister();
